918fa0684b08869c8127b88d4f69197e2b9b1c3c
Stefan Vasic <vasicstefan123@gmail.com>
2023-03-26 14:28:35 +0200

    Additional setup

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    //  const conn = await mongoose.connect(process.env.MONGO_URI);
-    const conn = await mongoose.connect('mongodb://mongo-db/sportera');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    //  const conn = await mongoose.connect('mongodb://mongo-db/sportera');
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
